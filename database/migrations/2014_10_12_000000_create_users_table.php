081ea7a285b8497c327ae023db3d2bffe27d0b3e
feranmi <feranmiolasunkanmi91@gmail.com>
2022-08-18 00:45:13 +0100

    working on tasks migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->enum('user_type', ['ADMIN', 'USER'])->default('USER');
             $table->string('api_token', 80)->nullable()->unique();
+            $table->string('task_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
