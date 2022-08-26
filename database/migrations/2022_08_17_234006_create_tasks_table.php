42e8b7a90af6cee725c08cee08f9b4eef462a9ac
feranmi <feranmiolasunkanmi91@gmail.com>
2022-08-26 01:19:52 +0100

    Migration fix

--- database/migrations/2022_08_17_234006_create_tasks_table.php
+++ database/migrations/2022_08_17_234006_create_tasks_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('description');
             $table->string('image');
-            $table->string('user_id');
+            // $table->string('user_id');
             $table->timestamps();
         });
     }
